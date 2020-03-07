9669684b4e9b68573500be8664578c34079bc47b
Dan Park <dpark.public@gmail.com>
2020-03-07 11:30:55 -0800

    Initial Commit

---
+++ readstamp/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  readstamp
+//
+//  Created by Dan Park on 3/7/20.
+//  Copyright © 2020 Dan Park. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
