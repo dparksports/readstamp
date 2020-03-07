8ffeff8d30f290752400f05e37f2b5ffbbe541f4
Dan Park <dpark.public@gmail.com>
2020-03-07 11:50:38 -0800

    created a readstamp command line.

--- readstamp/main.m
+++ readstamp/main.m
@@ -6,11 +6,24 @@
 //  Copyright © 2020 Dan Park. All rights reserved.
 //
 
+#import <Cocoa/Cocoa.h>
 #import <Foundation/Foundation.h>
+#import "SLTesseract.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
+        NSString *pictureDirectory = [NSSearchPathForDirectoriesInDomains(NSPicturesDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+        NSString *filename = [pictureDirectory stringByAppendingFormat:@"/f90182848.jpg", index];
+        NSImage *image = [[NSImage alloc] initWithContentsOfFile:filename];
+        
+        //ocr.charWhitelist = @"1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+        //ocr.charWhitelist = @"1234567890";
+        //ocr.charBlacklist = @"1234567890";
+        SLTesseract *ocr = [[SLTesseract alloc] init];
+        ocr.language = @"eng";
+        NSString *text = [ocr recognize:image];
+        printf("Text detected: \n%s\n", [text UTF8String]);
+
         NSLog(@"Hello, World!");
     }
     return 0;
